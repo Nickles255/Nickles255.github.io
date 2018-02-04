8d8334f8b29bf6b757d39bc8687c5ac233bc914b
clin <Nickles255@gmail.com>
2018-02-04 01:06:36 +0000

    HW2 completed moved js to hw2_scripts.js and styling to hw_link_pages.css

--- README.md
+++ README.md
@@ -6,12 +6,18 @@ Spring 2018.
 
     Homework 1: Done 1/18/2018 Initialize home page with html and css.
     
-    Homework 2: 1/26/2018 completed styling of main HW pages.
+    Homework 2: 1/26/2018 completed styling of main HW pages. 
+                Done on 2/3/2017 working html on each page. 
+                  Moved all Javascript to hw2_scripts.js
+                  Created basuc css customizations in hw_link_pages.css.
+                  added many css required for hw2 to the specifications.:
     PT1 - document write
     PT2 - Input 3 integers
     PT3 - jQuery
     PT4 - Calculation Table
     EX CRED - Exchange Rate.
+
+
 
     local drive location is:
         C:/Users/Acer/Dropbox/Courses/Javascript/CNIT133/webpage/index.htm
--- hw2/hw2_scripts.js
+++ hw2/hw2_scripts.js
@@ -1,6 +1,30 @@
 // Javascript related to Homework 2.
 // author: clin date: 2018-02-01
 
+//Function createNavMenu creates the navigation menu to the current homework assignment so all html pages will have to same links.
+
+function createNavMenu(){
+    document.write('<ul>');
+    document.write('<li><a class="mainpg" href="../index.html">CLIN Page</a></li>');
+    document.write('<li><a href="hw2.html">HW2 Page</a></li>');
+    document.write('<li><a href="hw2_pt1.html">Part 1</a></li>');
+    document.write(' <li><a href="hw2_pt2.html">Part 2</a></li>');
+    document.write('<li><a href="hw2_pt3.html">Part 3</a></li>');
+    document.write('<li><a href="hw2_pt4.html">Part 4</a></li>');
+    document.write('<li><a href="hw2_pt5.html">Extra Credit</a></li>');
+    document.write('</ul>');
+}
+
+//PART 1
+//Function dispVal displays document write dispays the three values 
+// required on the screen
+function dispVal(){
+    document.write("<span style = 'color: red;   font-family: \"Times New Roman\";'> ab </span>"); 
+    document.write("<span style = 'color: blue;  font-family: Arial;'>               12 </span>"); 
+    document.write("<span style = 'color: green; font-family: Impact;'>     <em>34</em> </span>"); 
+}
+
+
 // PART 2
 // Function calcNumSummary takes three numbers input by users in 
 // webpage and calculates sum, average, product, 
@@ -76,13 +100,44 @@ function calcPosNegZero(){
 // with row consisting of numbers, squares, and cubes starting at 1
 // and up to n.
 function drawPowerTbl(n){
-    document.write("<table class=\"powerTbl\">")
-    document.write("<tr><th>Number</th><th>Square</th><th>Cube</th></tr>")
+    document.write("<table class=\"powerTbl\">");
+    document.write("<tr><th>Number</th><th>Square</th><th>Cube</th></tr>");
     for(var curNum = 1; curNum <= n; curNum++){
-        document.write("<tr><td>" + curNum + "</td><td>" +
-                        Math.pow(curNum, 2) + "</td><td>" + 
-                        Math.pow(curNum, 3) + "</td></tr>");
+    document.write("<tr><td class=\"nums\">" + curNum + 
+                   "</td><td class=\"nums\">" + Math.pow(curNum, 2) +
+                   "</td><td class=\"nums\">" + Math.pow(curNum, 3) + 
+                   "</td></tr>");
     }
-    document.write("</table>")
+    document.write("</table>");
     
 }
+
+// PART 5
+// Function calcExchRate takes the input from document element "uDollar_val" and then cycles through a
+// table, from second row to second to last row, looking at the second column in each row
+// multiplies value it finds with the input amd output to the third column
+function calcExchRate(){
+    // from https://stackoverflow.com/questions/3072233/getting-value-from-table-cell-in-javascript-not-jquery
+    var myTable, uDollar, exchVal;
+    
+    uDollar = parseFloat(document.getElementById("uDollar_val").value);
+    myTable=document.getElementById("currencyTbl");
+    
+    for (var r = 1, nRow = myTable.rows.length -1; r < nRow; r++){
+        exchVal=myTable.rows[r].cells[1].innerHTML;
+        myTable.rows[r].cells[2].innerHTML = (exchVal * uDollar).toFixed(4);
+    } 
+}
+
+// function below are used in the extra credit document to create functionaliy.
+    $(document).ready(function(){
+        // when the focus is on the box id'd as uDollar_val then the background color of the text box will turn green
+        $("#uDollar_val").focus(function(){
+            $(this).css("background-color", "lightgreen");
+        });
+        
+        // when the element id'd as flip is clicked the element id'd as describe will appear or disappear as toggled.
+        $("#flip").click(function(){
+            $("#describe").slideToggle("slow");
+        });
+    });
