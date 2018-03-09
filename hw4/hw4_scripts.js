407944c6ba5d7f8a4973d83e40ddf0fa6f466418
clin <Nickles255@gmail.com>
2018-03-09 13:40:07 +0000

    Set up HW4 and set up folders for HW5 through HW8

---
+++ hw4/hw4_scripts.js
@@ -0,0 +1,22 @@
+// Javascript related to Homework 3.
+// author: clin date: 2018-02-11
+
+//Function createNavMenu creates the navigation menu to the current homework assignment so all html pages will have to same links.
+
+function createNavMenu(){
+    document.write('<nav class="menu">')
+    document.write('<ul>');
+    document.write('<li><a class="mainpg" href="../index.html">CLIN Page</a></li>');
+    document.write('<li><a href="hw3.html">HW4 Page</a></li>');
+    document.write('<li><a href="hw4_pt1.html">' + 
+                   'Part 1 - For Statement product and sums</a></li>');
+    document.write(' <li><a href="hw4_pt2.html">' + 
+                   'Part 2 - Calculation of compound interest</a></li>');
+    document.write('<li><a href="hw4_pt3.html">' +
+                   'Part 3 - Retail sales</a></li>');
+    document.write('<li><a href="hw4_pt4.html">' + 
+                   'Extra Credit - Square Shapes</a></li>');
+    document.write('</ul>');
+    document.write('</nav>')
+}
+
---
+++ hw6/hw6_scripts.js
@@ -0,0 +1,22 @@
+// Javascript related to Homework 3.
+// author: clin date: 2018-02-11
+
+//Function createNavMenu creates the navigation menu to the current homework assignment so all html pages will have to same links.
+
+function createNavMenu(){
+    document.write('<nav class="menu">')
+    document.write('<ul>');
+    document.write('<li><a class="mainpg" href="../index.html">CLIN Page</a></li>');
+    document.write('<li><a href="hw3.html">HW3 Page</a></li>');
+    document.write('<li><a href="hw3_pt1.html">' + 
+                   'Part 1 - Calc Student Grades</a></li>');
+    document.write(' <li><a href="hw3_pt2.html">' + 
+                   'Part 2 - Weekly Pay for Sales</a></li>');
+    document.write('<li><a href="hw3_pt3.html">' +
+                   'Part 3 - Temp Conversion</a></li>');
+    document.write('<li><a href="hw3_pt4.html">' + 
+                   'Part 4 - Simple Math Practice</a></li>');
+    document.write('</ul>');
+    document.write('</nav>')
+}
+
