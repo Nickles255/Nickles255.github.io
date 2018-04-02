010815d6cc6761cbaf710afc95eaed4103d294cf
clin <clin@ccsf.edu>
2018-04-02 06:03:55 +0100

    Completed HW5 moved all scripts to hw5_script.js

--- hw5/hw5_scripts.js
+++ hw5/hw5_scripts.js
@@ -18,3 +18,88 @@ function createNavMenu(){
     document.write('</nav>')
 }
 
+//Part 1. Development of form validation 
+//STEP 1. set rules for each named section of form.
+//STEP 2. create messages for each named section of form.
+//STEP 3. errorContainer assigned the location to place all error messages.
+$(document).ready(function(){
+    $("#pInterest").validate({
+        rules: {
+            fullName: {required: true},
+            gender:   {required: true},
+            interest: {required: true},
+            selTime:  {required: true}
+        },
+        messages: {
+            fullName: "Please enter your name",
+            gender:   "Please select a gender you identify with",
+            interest: "Please select one sport which interest you",
+            selTime:  "Please chose a time which is convenient for you"
+        },
+        submitHandler: function(form) {
+            form.submit();
+        },
+        errorContainer: $('#errorContainer'),
+        errorLabelContainer: $('#errorContainer'),
+        wrapper: 'li'
+    });
+});
+
+//PART 2. event handler in pull down menu executes this function
+//        for both button and onchange parameter.
+//STEP 1. identify selected item of destList menu. assign to destination target.
+//STEP 2. set window location to destination identified in STEP 1.
+function goToNewPage(myForm) {
+    var myDestination = myForm.destList.options[myForm.destList.selectedIndex].value;
+    window.location.href = myDestination;
+}
+
+/* PART 3.STATE DATA
+   Author: CLIN
+   DATE: 2018-03-31 
+   PURPOSE: Using two dimensional array of state data.
+   Need to identify state by name or abbreviate (case insensitive)
+   
+   STEP 1. Create two dimensional array each state is a row with 4 columns
+           state abbreviation, state name, state capitol, and population.
+   STEP 2. Create searchable single dimension array of search terms 
+   STEP 3. take user input and validate return name capitol and population in text fields.
+*/
+var myState = [['AL', 'Alabama', 'Montgomery', 4779736],
+               ['AK', 'Akaska', 'Juneau', 710231],
+               ['AZ', 'Arizona', 'Phoenix', 6392017],
+               ['AR', 'Arkansas', 'Little Rock', 2915918],
+               ['CA', 'California', 'Sacramento', 37253956],
+               ['CO', 'Colorado', 'Denver', 5029196]         
+              ];
+// creating search array from first two columns in myState array with all characters upper case.
+// https://stackoverflow.com/questions/7848004/get-column-from-a-two-dimensional-array-in-javascript
+var searchArray = myState.map(x => x[0]).concat(myState.map(x => x[1]));
+searchArray = searchArray.map(function(x){return x.toUpperCase()})
+// jquery validation and mapping of a single column reference.
+// https://stackoverflow.com/questions/5696222/jquery-validator-check-input-against-a-list-of-accepted-values              
+jQuery.validator.addMethod("isstate", function(value) {
+            return $.inArray(value.toUpperCase(), searchArray) != -1;
+            }, "Data provided is not a valid state");
+
+// function to populate the state elements if the stateForm is validated.
+function getStateData(pState){
+    if($('#stateForm').valid()){
+        var stateIndex = searchArray.indexOf(pState.toUpperCase())%myState.length
+        document.forms["stateForm"].elements["stateNameOut"].value = myState[stateIndex][1]
+        document.forms["stateForm"].elements["stateCapitolOut"].value = myState[stateIndex][2]
+        document.forms["stateForm"].elements["statePopOut"].value = 
+            Number(myState[stateIndex][3]).toLocaleString() 
+    }
+};
+
+$(document).ready(function(){
+        $("#stateForm").validate();
+        $("#dispInst").hide();
+        $("#instButton").click(function(){
+                            $("#dispInst").show();
+                            $("#dispInst").delay(4000).fadeOut('slow');
+                            });
+
+}); 
+
